349c9877f53fdcbc577dc4e1a7bf210740972a84
Marko <markotaskovic93@gmail.com>
2018-11-10 16:21:32 +0100

    dodat home page

---
+++ application/views/home.php
@@ -0,0 +1,30 @@
+
+<!--// Navbar section -->
+<div class="container header">
+	<div class="row">
+		<div class="col-12">
+			<nav class="navbar navbar-expand-lg">
+				<a class="navbar-brand" href="#">WebApp</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">Disabled</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+</div><!--// End of Navbar section -->

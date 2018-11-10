db5647cd9e5fddd692e30fc74fac525fac6ff8eb
Marko <markotaskovic93@gmail.com>
2018-11-10 19:48:01 +0100

    Home page

---
+++ da/home.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WebsApp</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="assets/css/home-page.css">
+	<link rel="stylesheet" href="assets/library/bootstrap/css/bootstrap.min.css">
+	<script src="assets/library/jquery/jquery.min.js"></script>
+	<script src="assets/library/bootstrap/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"> 
+
+</head>
+<body>
+
+	<!--// Navbar section -->
+	<div class="container-fluid header">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<nav class="navbar navbar-expand-lg">
+							<a class="navbar-brand" href="#">WebApp</a>
+							<ul class="navbar-nav ml-auto">
+								<li class="nav-item">
+									<button class="btn btn-primary log-in" data-toggle="modal" data-target="#exampleModal">Log In</button>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!--// End of Navbar section -->
+
+
+	<!--// Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="col-12 text-center modall-header">
+					<h5>Ulogujte se</h5>
+				</div>
+				<div class="modal-body">
+
+					<form action="#" method="POST">
+
+						<label for="username">Korisnicko Ime</label>
+						<input class="form-control" type="text" name="username" required>
+						<br>
+						<label for="pwd">Lozinka</label>
+						<input class="form-control" type="text" name="password" required>
+						<br>
+						<input class="btn btn-primary" type="submit" value="Log In">
+
+					</form>
+
+				</div>
+			</div>
+		</div>
+	</div><!--// End of Modal -->
+
+
+	<div class="container-fluid front-image" style="background-image: url('assets/images/doctor.jpg');">
+		<div class="shade">
+			<div class="row h-100">
+				<div class="container align-self-center">
+					<div class="row">
+						<div class="col-12 col-md-6 text-center text-md-left front-text">
+							<h1>Neki Tekst</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut obcaecati, rem adipisci in consequuntur hic, cum eaque suscipit. consectetur adipisicing elit. Ut obcaecati, rem adipisci in consequuntur hic, cum eaque suscipit.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container services">
+		<div class="row">
+			<div class="col-12 col-md-4 text-center service">
+				<i class="fas fa-heartbeat"></i>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur sequi tenetur odio debitis, doloremque quidem esse minus rerum. Doloribus iste cumque sed molestias amet in nemo illum totam provident id.</p>
+			</div>
+			<div class="col-12 col-md-4 text-center service">
+				<i class="fas fa-ambulance"></i>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur sequi tenetur odio debitis, doloremque quidem esse minus rerum. Doloribus iste cumque sed molestias amet in nemo illum totam provident id.</p>
+			</div>
+			<div class="col-12 col-md-4 text-center service">
+				<i class="fas fa-calendar-plus"></i>
+				<h3>Title</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur sequi tenetur odio debitis, doloremque quidem esse minus rerum. Doloribus iste cumque sed molestias amet in nemo illum totam provident id.</p>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="container-fluid paralax" style="background-image: url('assets/images/lab.png');">
+		<div class="shade1">
+			<div class="row h-100">
+				<div class="container align-self-center">
+					<div class="row">
+						<div class="col-12 text-center">
+							<h3>Lorem ipsum dolor sit amet</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil a delectus, ratione necessitatibus sed, fuga molestias dolorem consequuntur, fugit molestiae consequatur, quisquam quam? Aut blanditiis minus dolorem ratione laborum ut.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+	<div class="container-fluid doctors">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-sm-6 col-md-3 doctor">
+						<img class="img-fluid" src="assets/images/doctor1.jpg" alt="">
+						<h5>Stiven Darlington</h5>
+						<p><i>-- Dermatolog</i></p>
+					</div>
+					<div class="col-12 col-sm-6 col-md-3 doctor">
+						<img class="img-fluid" src="assets/images/doctor2.jpg" alt="">
+						<h5>Marko Taskovic</h5>
+						<p><i>-- Dermatolog</i></p>
+					</div>
+					<div class="col-12 col-sm-6 col-md-3 doctor">
+						<img class="img-fluid" src="assets/images/doctor3.jpg" alt="">
+						<h5>Skocko nzm ime</h5>
+						<p><i>-- Dermatolog</i></p>
+					</div>
+					<div class="col-12 col-sm-6 col-md-3 doctor">
+						<img class="img-fluid" src="assets/images/doctor4.jpg" alt="">
+						<h5>Lazar Stamenkovic</h5>
+						<p><i>-- Dermatolog</i></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div class="container-fluid footer">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-12 text-center text-md-left">
+						<p>All Right Reserved <b style="color: #fff;">PMF TLE Team</b> 2018 Hakaton</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+

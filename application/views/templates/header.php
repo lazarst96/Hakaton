505750ec573c7d6fccb52cdd8ae2a8ff16f2a753
Marko <markotaskovic93@gmail.com>
2018-11-10 16:25:52 +0100

    header ubacen

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 	<title><?=$title?>::DermaReport</title>
+	<?php foreach ($scripts as $link):?>
+		<script src="<?=$link?>"></script>
+	<?php endforeach?>
 	<?php foreach ($styles as $link):?>
-	<link rel="stylesheet" type="text/css" href="<?=$link?>">
+		<link rel="stylesheet" type="text/css" href="<?=$link?>">
 	<?php endforeach?>
 </head>
 <body>
